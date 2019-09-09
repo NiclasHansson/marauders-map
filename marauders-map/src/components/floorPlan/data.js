69a609ce8e36318a57bf68c36f0964a99beed03b
Fredrik Sundman <Sundman73@gmail.com>
2019-09-09 10:07:09 +0200

    Common Datastructure for rooms.

---
+++ marauders-map/src/components/floorPlan/data.js
@@ -0,0 +1,62 @@
+export const Rooms = [
+    {
+        label: 'Elevator/Staircase',
+        floor: 8,
+        type: 'stairway',
+        coordinates: [70, 25],
+        data: {},
+    },
+    {
+        label: "Alp d'Huez",
+        floor: 8,
+        type: 'meeting',
+        coordinates: [25, 10],
+        data: {
+            size: 2,
+        },
+    },
+    {
+        label: 'Alta',
+        floor: 8,
+        type: 'meeting',
+        coordinates: [35, 10],
+        data: {
+            size: 4,
+        },
+    },
+    {
+        label: 'Aspen',
+        floor: 8,
+        type: 'meeting',
+        coordinates: [15, 10],
+        data: {
+            size: 8,
+        },
+    },
+    {
+        label: 'Chamonix',
+        floor: 8,
+        type: 'meeting',
+        coordinates: [45, 10],
+        data: {
+            size: 14,
+        },
+    },
+    {
+        label: 'bathroom1',
+        floor: 8,
+        type: 'bathroom',
+        coordinates: [20, 30],
+        data: {},
+    },
+    {
+        label: 'fika1',
+        floor: 8,
+        type: 'kitchen',
+        coordinates: [35, 65],
+        data: {},
+    },
+];
+
+
+export default Rooms;
