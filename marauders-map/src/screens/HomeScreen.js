68aff39ad1f39a2448d79f63bc275be1f11210d7
Niclas Hansson <Niclas.Hansson.ext@netent.com>
2019-09-09 14:25:08 +0200

    init video intro not working

--- marauders-map/src/screens/HomeScreen.js
+++ marauders-map/src/screens/HomeScreen.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
+import Video from 'react-native-af-video-player';
 
 import Colors from '../constants/Colors';
 import FloorPlan from '../components/floorPlan/FloorPlan';
+
+const video = require('../../assets/videos/intro.mp4');
 
 const styles = StyleSheet.create({
     container: {
@@ -12,6 +15,13 @@ const styles = StyleSheet.create({
     scrollContainer: {
         flex: 1,
     },
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    },
 });
 
 export default function HomeScreen() {
@@ -19,6 +29,7 @@ export default function HomeScreen() {
         <View style={styles.container}>
             <ScrollView contentContainerStyle={styles.scrollContainer}>
                 <FloorPlan />
+                <Video url={video} />
             </ScrollView>
         </View>
     );
