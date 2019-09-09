06834a990976d387859d0d6a85eeb9051ddf8d20
Fredrik Sundman <Sundman73@gmail.com>
2019-09-09 11:50:42 +0200

    Rumsdata för plan 8 ännu ej korrekt placering.

--- marauders-map/src/components/floorPlan/data.js
+++ marauders-map/src/components/floorPlan/data.js
@@ -1,14 +1,33 @@
-export const Rooms = [
+﻿export const Rooms = [
     {
-        label: 'Elevator/Staircase',
+        label: 'All',
         floor: 8,
-        type: 'stairway',
-        coordinates: [70, 25],
-        data: {},
+        type: 'all',
+        coordinates: [-1000, 0],
+    },
+    {
+        label: 'Dalaköket',
+        floor: 8,
+        type: 'kitchen',
+        coordinates: [80, 27],
+    },
+    {
+        label: "Torsköket",
+        floor: 8,
+        type: 'kitchen',
+        coordinates: [25, 25],
+        data: {
+            size: 30,
+            projector: false,
+            tv: false,
+            phone: false,
+            skype: false,
+            other: '',
+        },
     },
     {
         label: "Alp d'Huez",
-        floor: 8,
+        floor: 2,
         type: 'meeting',
         coordinates: [25, 10],
         data: {
@@ -19,43 +38,358 @@ export const Rooms = [
         label: 'Alta',
         floor: 8,
         type: 'meeting',
-        coordinates: [35, 10],
+        coordinates: [87, 45],
         data: {
-            size: 4,
+            size: 2,
+            projector: false,
+            tv: false,
+            phone: true,
+            skype: false,
+            other: '',
         },
     },
     {
         label: 'Aspen',
         floor: 8,
         type: 'meeting',
-        coordinates: [15, 10],
-        data: {
-            size: 8,
-        },
-    },
-    {
-        label: 'Chamonix',
-        floor: 8,
-        type: 'meeting',
-        coordinates: [45, 10],
+        coordinates: [25, 10],
         data: {
             size: 14,
+            projector: true,
+            tv: false,
+            phone: true,
+            skype: false,
+            other: '',
         },
     },
     {
-        label: 'bathroom1',
+        label: 'Banff',
+        floor: 8,
+        type: 'meeting',
+        coordinates: [25, 10],
+        data: {
+            size: 6,
+            projector: false,
+            tv: true,
+            phone: true,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: "Chamonix",
+        floor: 8,
+        type: 'meeting',
+        coordinates: [75, 20],
+        data: {
+            size: 15,
+            projector: true,
+            tv: false,
+            phone: true,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: "Cortina",
+        floor: 8,
+        type: 'meeting',
+        coordinates: [45, 35],
+        data: {
+            size: 2,
+            projector: false,
+            tv: false,
+            phone: true,
+            skype: false,
+        },
+    },
+    {
+        label: 'Fernie',
+        floor: 8,
+        type: 'meeting',
+        coordinates: [25, 10],
+        data: {
+            size: 12,
+            projector: true,
+            tv: false,
+            phone: true,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: 'Hemsedal',
+        floor: 8,
+        type: 'meeting',
+        coordinates: [57, 10],
+        data: {
+            size: 12,
+            projector: true,
+            tv: false,
+            phone: true,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: 'Kitsbuhel',
+        floor: 8,
+        type: 'meeting',
+        coordinates: [25, 10],
+        data: {
+            size: 2,
+            projector: false,
+            tv: false,
+            phone: true,
+            skype: false,
+            other: 'No whiteboard',
+        },
+    },
+    {
+        label: "St Anton",
+        floor: 8,
+        type: 'meeting',
+        coordinates: [50, 85],
+        data: {
+            size: 14,
+            projector: true,
+            tv: false,
+            phone: true,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: 'Stryn',
+        floor: 8,
+        type: 'meeting',
+        coordinates: [25, 10],
+        data: {
+            size: 4,
+            projector: false,
+            tv: false,
+            phone: true,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: "Sölden",
+        floor: 8,
+        type: 'meeting',
+        coordinates: [45, 60],
+        data: {
+            size: 4,
+            projector: false,
+            tv: false,
+            phone: true,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: "Teamrum DIGI Management",
+        floor: 8,
+        type: 'teamroom',
+        coordinates: [15, 20],
+        data: {
+            size: 4,
+            projector: false,
+            tv: false,
+            phone: false,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: "Teamrum Yttrium/Lead fokus",
+        floor: 8,
+        type: 'teamroom',
+        coordinates: [30, 25],
+        data: {
+            size: 4,
+            projector: false,
+            tv: false,
+            phone: false,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: "Teamrum Yttrium/Lead Stå",
+        floor: 8,
+        type: 'teamroom',
+        coordinates: [35, 25],
+        data: {
+            size: 4,
+            projector: false,
+            tv: true,
+            phone: false,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: "Teamrum Yttrium/Lead Sitta",
+        floor: 8,
+        type: 'teamroom',
+        coordinates: [40, 25],
+        data: {
+            size: 4,
+            projector: false,
+            tv: true,
+            phone: false,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: "Teamrum ROAD Verbier",
+        floor: 8,
+        type: 'teamroom',
+        coordinates: [60, 20],
+        data: {
+            size: 2,
+            projector: false,
+            tv: true,
+            phone: false,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: "Teamrum ROAD Morzine",
+        floor: 8,
+        type: 'teamroom',
+        coordinates: [65, 20],
+        data: {
+            size: 2,
+            projector: false,
+            tv: true,
+            phone: false,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: "Teamrum ROAD Val d'Isere",
+        floor: 8,
+        type: 'teamroom',
+        coordinates: [70, 20],
+        data: {
+            size: 2,
+            projector: false,
+            tv: true,
+            phone: false,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: "Tignes",
+        floor: 8,
+        type: 'meeting',
+        coordinates: [60, 25],
+        data: {
+            size: 8,
+            projector: true,
+            tv: false,
+            phone: false,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: 'Vail',
+        floor: 8,
+        type: 'meeting',
+        coordinates: [87, 55],
+        data: {
+            size: 7,
+            projector: false,
+            tv: true,
+            phone: true,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: "Åre",
+        floor: 8,
+        type: 'meeting',
+        coordinates: [15, 15],
+        data: {
+            size: 12,
+            projector: true,
+            tv: false,
+            phone: true,
+            skype: false,
+            other: '',
+        },
+    },
+    {
+        label: 'Bathroom',
         floor: 8,
         type: 'bathroom',
         coordinates: [20, 30],
         data: {},
     },
     {
-        label: 'fika1',
+        label: 'Bathroom',
         floor: 8,
-        type: 'kitchen',
-        coordinates: [35, 65],
+        type: 'bathroom',
+        coordinates: [25, 10],
         data: {},
     },
+    {
+        label: 'Printer',
+        floor: 8,
+        type: 'printer',
+        coordinates: [25, 10],
+        data: {},
+    },
+    {
+        label: 'Printer',
+        floor: 8,
+        type: 'printer',
+        coordinates: [25, 10],
+        data: {},
+    },
+    {
+        label: "Printer",
+        floor: 8,
+        type: 'printer',
+        coordinates: [20, 25],
+        data: {},
+    },
+    {
+        label: "Printer",
+        floor: 8,
+        type: 'printer',
+        coordinates: [80, 30],
+        data: {},
+    }
 ];
 
 export default Rooms;
+
+
+    
+
+
+    
+
+        
+
+        
+            
+
+            
+
+                
+
+                
+
+                    
+
+                            
